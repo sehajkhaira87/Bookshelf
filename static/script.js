2be053480647e8de63c6c7395d03d80b9616a923
sehajkhaira87 <sehajqq@gmail.com>
2026-07-09 16:55:36 +0530

    made the logo for the website and changed the positions for the coordinates

--- static/script.js
+++ static/script.js
@@ -2,4 +2,4 @@ const loader = document.getElementById("loader");
 
 setTimeout(() => {
     loader.classList.add("hide-loader");
-}, 1000);
+}, 1500); // Hide the loader after 1.5 seconds // 1000 means 1 second only 
